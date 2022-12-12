05fd6c2a9f9d60090b44c865bff51104e97ddce8
cccccccccccccccccccccccccccccccccccccc <linus3389@gmail.com>
2022-12-12 11:10:19 +0800

    asdasdsad

---
+++ api/proxy.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+var URLtoProxy = os.Getenv("https://admin.airbridgedev.com/")
+
+func ProxyHandler(w http.ResponseWriter, r *http.Request) {
+	client := &http.Client{}
+	var url = URLtoProxy + r.URL.Path
+	if len(r.URL.Query()) > 0 {
+		url += "?" + r.URL.RawQuery
+	}
+	req, err := http.NewRequest(r.Method, url, r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+	for k, v := range resp.Header {
+		w.Header().Set(k, v[0])
+	}
+	w.WriteHeader(resp.StatusCode)
+	resBody, err := ioutil.ReadAll(resp.Body)
+	w.Write(resBody)
+}
